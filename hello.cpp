1f22d7aac0d686573820d925f3ced96970ac5457
shiba <171860526@smail.nju.edu.cn>
2018-05-03 13:13:34 +0800

    hello world

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x,y;
-    cout<<"请输入两个数字:";
-    cin>>x>>y;
-    cout<<x<<'+'<<y<<'='<<x+y<<endl;
-    cout<<x<<'-'<<y<<'='<<x-y<<endl;
     cout<<"hello world"<<endl;
     return 0;
 }
